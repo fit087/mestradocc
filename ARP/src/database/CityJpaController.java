0541fd692c208d75fbbfc985f83ed8d400c31adc
alexander.de.almeida.pinto@gmail.com <alexander.de.almeida.pinto@gmail.com@f83dd330-27a4-11da-b4eb-a8d1c6e47ee3>
2010-08-02 18:48:44 +0000

    Refatoracao no nome do projeto

---
+++ ARP/src/database/CityJpaController.java
@@ -0,0 +1,140 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package database;
+
+import database.exceptions.NonexistentEntityException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import main.entities.City;
+
+/**
+ *
+ * Desenvolvido por: Alexander de Almeida Pinto
+ *
+ * @author alexanderdealmeidapinto
+ */
+public class CityJpaController {
+
+    public CityJpaController() {
+        emf = Persistence.createEntityManagerFactory("ARPPU");
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(City city) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(city);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(City city) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            city = em.merge(city);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = city.getId();
+                if (findCity(id) == null) {
+                    throw new NonexistentEntityException("The city with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            City city;
+            try {
+                city = em.getReference(City.class, id);
+                city.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The city with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(city);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<City> findCityEntities() {
+        return findCityEntities(true, -1, -1);
+    }
+
+    public List<City> findCityEntities(int maxResults, int firstResult) {
+        return findCityEntities(false, maxResults, firstResult);
+    }
+
+    private List<City> findCityEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(City.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public City findCity(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(City.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getCityCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<City> rt = cq.from(City.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
