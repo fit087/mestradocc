4f32d2a74715ff86f1c5e4f83e2a1c1d687c11fb
alexander.de.almeida.pinto@gmail.com <alexander.de.almeida.pinto@gmail.com@f83dd330-27a4-11da-b4eb-a8d1c6e47ee3>
2010-08-04 23:21:53 +0000

    Resolvido o problema de JPA

---
+++ AIRotation/src/main/entities/City.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main.entities;
+
+import java.util.HashMap;
+
+/**
+ *
+ * Desenvolvido por: Alexander de Almeida Pinto
+ *
+ * @author alexanderdealmeidapinto
+ */
+public class City  {
+
+    //Nome da cidade.
+    private String name;
+
+    //Tempo de solo
+    private Integer groundTime;
+
+    //Tempo de voo entre as cidades.
+    private HashMap<City, Integer> flightTimes = new HashMap<City, Integer>();
+
+    public City() {
+    }
+
+    public City(String name, Integer groundTime) {
+        this.name = name;
+        this.groundTime = groundTime;
+    }
+
+    public HashMap<City, Integer> getFlightTimes() {
+        return flightTimes;
+    }
+
+    public void setFlightTimes(HashMap<City, Integer> flightTimes) {
+        this.flightTimes = flightTimes;
+    }
+
+    public Integer getGroundTime() {
+        return groundTime;
+    }
+
+    public void setGroundTime(Integer groundTime) {
+        this.groundTime = groundTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof City)) {
+            return false;
+        }
+        City other = (City) object;
+        
+        if(other.getName().equals(getName())) {
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + name + " GroundTime: " + groundTime;
+    }
+
+
+    /**
+     * Adiciona o tempo de uma cidade apartir dessa.
+     * @param city
+     * @param time
+     */
+    public void addCityTime(City city, int time) {
+        this.flightTimes.put(city, time);
+    }
+
+}
